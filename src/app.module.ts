1c655fc098f97a39f278a2d822bde3b193684d87
Ben Yip <ben_yip@126.com>
2024-07-29 23:05:28 +0800

    Init configuration of typeorm for connecting postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PG_HOST: Joi.string().required(),
+        PG_PORT: Joi.number().required(),
+        PG_USERNAME: Joi.string().required(),
+        PG_PASSWORD: Joi.string().required(),
+        PG_DB: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: configService.get('PG_PORT'),
+        username: configService.get('PG_USERNAME'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
