8a4e6f820beff39c488ec73503a1a0064e1728b1
Ben Yip <ben_yip@126.com>
2024-10-19 13:49:59 +0800

    pino and config modules installed

---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { CreateUserRequest } from './dto/create-user.request';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  createUser(data: CreateUserRequest) {
+    console.log(data);
+  }
+}
